fc508f115c779a25e080a53e1f33d156c34e1e8b
Rocco Panacci <rocco@clausehound.com>
2017-08-17 13:31:16 -0400

    resolved page errors and setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,24 +10,12 @@ import { ChallengeList3Page } from '../pages/challenge-list3/challenge-list3';
 import { ChallengeList4Page } from '../pages/challenge-list4/challenge-list4';
 import { NextChallengePage } from '../pages/next-challenge/next-challenge';
 import { SmileAtSomeDudePage } from '../pages/smile-at-some-dude/smile-at-some-dude';
-import { SmileAtSomeDude2Page } from '../pages/smile-at-some-dude2/smile-at-some-dude2';
 import { QuickAddPage } from '../pages/quick-add/quick-add';
-import { QuickAdd2Page } from '../pages/quick-add2/quick-add2';
-import { QuickAdd3Page } from '../pages/quick-add3/quick-add3';
-import { QuickAdd4Page } from '../pages/quick-add4/quick-add4';
-import { QuickAdd5Page } from '../pages/quick-add5/quick-add5';
-import { QuickAdd6Page } from '../pages/quick-add6/quick-add6';
-import { QuickAdd7Page } from '../pages/quick-add7/quick-add7';
 import { ChallengeCompletePage } from '../pages/challenge-complete/challenge-complete';
-import { ChallengeComplete2Page } from '../pages/challenge-complete2/challenge-complete2';
-import { ChallengeComplete3Page } from '../pages/challenge-complete3/challenge-complete3';
-import { ChallengeComplete4Page } from '../pages/challenge-complete4/challenge-complete4';
-import { ChallengeComplete5Page } from '../pages/challenge-complete5/challenge-complete5';
 import { GoalsPage } from '../pages/goals/goals';
 import { PastChallengeDetailsPage } from '../pages/past-challenge-details/past-challenge-details';
 import { TipsPage } from '../pages/tips/tips';
 import { HomePage } from '../pages/home/home';
-import { SmileAtSomeDude3Page } from '../pages/smile-at-some-dude3/smile-at-some-dude3';
 import { MindfulBreathingPage } from '../pages/mindful-breathing/mindful-breathing';
 import { ExternalResourcesPage } from '../pages/external-resources/external-resources';
 
@@ -46,24 +34,12 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     ChallengeList4Page,
     NextChallengePage,
     SmileAtSomeDudePage,
-    SmileAtSomeDude2Page,
     QuickAddPage,
-    QuickAdd2Page,
-    QuickAdd3Page,
-    QuickAdd4Page,
-    QuickAdd5Page,
-    QuickAdd6Page,
-    QuickAdd7Page,
     ChallengeCompletePage,
-    ChallengeComplete2Page,
-    ChallengeComplete3Page,
-    ChallengeComplete4Page,
-    ChallengeComplete5Page,
     GoalsPage,
     PastChallengeDetailsPage,
     TipsPage,
     HomePage,
-    SmileAtSomeDude3Page,
     MindfulBreathingPage,
     ExternalResourcesPage
   ],
@@ -82,24 +58,12 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     ChallengeList4Page,
     NextChallengePage,
     SmileAtSomeDudePage,
-    SmileAtSomeDude2Page,
     QuickAddPage,
-    QuickAdd2Page,
-    QuickAdd3Page,
-    QuickAdd4Page,
-    QuickAdd5Page,
-    QuickAdd6Page,
-    QuickAdd7Page,
     ChallengeCompletePage,
-    ChallengeComplete2Page,
-    ChallengeComplete3Page,
-    ChallengeComplete4Page,
-    ChallengeComplete5Page,
     GoalsPage,
     PastChallengeDetailsPage,
     TipsPage,
     HomePage,
-    SmileAtSomeDude3Page,
     MindfulBreathingPage,
     ExternalResourcesPage
   ],
--- src/pages/goals/goals.ts
+++ src/pages/goals/goals.ts
@@ -5,13 +5,7 @@ import { ChallengeListPage } from '../challenge-list/challenge-list';
 import { ChallengeList2Page } from '../challenge-list2/challenge-list2';
 import { ChallengeList3Page } from '../challenge-list3/challenge-list3';
 import { SmileAtSomeDudePage } from '../smile-at-some-dude/smile-at-some-dude';
-import { SmileAtSomeDude2Page } from '../smile-at-some-dude2/smile-at-some-dude2';
-import { SmileAtSomeDude3Page } from '../smile-at-some-dude3/smile-at-some-dude3';
 import { ChallengeCompletePage } from '../challenge-complete/challenge-complete';
-import { ChallengeComplete2Page } from '../challenge-complete2/challenge-complete2';
-import { ChallengeComplete3Page } from '../challenge-complete3/challenge-complete3';
-import { ChallengeComplete4Page } from '../challenge-complete4/challenge-complete4';
-import { ChallengeComplete5Page } from '../challenge-complete5/challenge-complete5';
 import { NextChallengePage } from '../next-challenge/next-challenge';
 import { ChallengeList4Page } from '../challenge-list4/challenge-list4';
 import { PastChallengeDetailsPage } from '../past-challenge-details/past-challenge-details';
@@ -40,27 +34,9 @@ export class GoalsPage {
   }goToSmileAtSomeDude(params){
     if (!params) params = {};
     this.navCtrl.push(SmileAtSomeDudePage);
-  }goToSmileAtSomeDude2(params){
-    if (!params) params = {};
-    this.navCtrl.push(SmileAtSomeDude2Page);
-  }goToSmileAtSomeDude3(params){
-    if (!params) params = {};
-    this.navCtrl.push(SmileAtSomeDude3Page);
   }goToChallengeComplete(params){
     if (!params) params = {};
     this.navCtrl.push(ChallengeCompletePage);
-  }goToChallengeComplete2(params){
-    if (!params) params = {};
-    this.navCtrl.push(ChallengeComplete2Page);
-  }goToChallengeComplete3(params){
-    if (!params) params = {};
-    this.navCtrl.push(ChallengeComplete3Page);
-  }goToChallengeComplete4(params){
-    if (!params) params = {};
-    this.navCtrl.push(ChallengeComplete4Page);
-  }goToChallengeComplete5(params){
-    if (!params) params = {};
-    this.navCtrl.push(ChallengeComplete5Page);
   }goToNextChallenge(params){
     if (!params) params = {};
     this.navCtrl.push(NextChallengePage);
